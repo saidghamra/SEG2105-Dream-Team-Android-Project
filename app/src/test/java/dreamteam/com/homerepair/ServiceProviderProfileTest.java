ed0563584fb9538ea461ab21d59a44d6b709e17d
santosgagbegnon <sgagb074@uottawa.ca>
2018-11-19 17:37:22 -0500

    added service provider tests

---
+++ app/src/test/java/dreamteam/com/homerepair/ServiceProviderProfileTest.java
@@ -0,0 +1,18 @@
+package dreamteam.com.homerepair;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceProviderProfileTest {
+    @Test
+    public void testServiceProviderProfileGetID() {
+        ServiceProviderProfile serviceProviderProfile = new ServiceProviderProfile("ab3f", null, null, null, false, null, null);
+        assertEquals("Testing Service Provider Profile's getID", "abc3f",serviceProviderProfile.getId());
+    }
+    @Test
+    public void testServiceProviderProfileGetAddress() {
+        ServiceProviderProfile serviceProviderProfile = new ServiceProviderProfile(null, "123Street", null, null, false, null, null);
+        assertEquals("Testing Service Provider Profile's getAddress", "123Street",serviceProviderProfile.getAddress());
+    }
+}
--- app/src/test/java/dreamteam/com/homerepair/UserTest.java
+++ app/src/test/java/dreamteam/com/homerepair/UserTest.java
@@ -5,19 +5,19 @@ public class UserTest {
 
     @Test
     public void testUserGetName(){
-        User user1 = new User( "Admin", "tim_cook",  "123456");
+        User user1 = new User( "1","Admin", "tim_cook",  "123456");
         assertEquals("Testing User's getUsername", "tim_cook",user1.getUsername());
     }
 
     @Test
     public void testUserGetAccountType(){
-        User user1 = new User( "Admin", "tim_cook",  "123456");
+        User user1 = new User( "1","Admin", "tim_cook",  "123456");
         assertEquals("Testing User's getAccountType", "Admin",user1.getAccountType());
     }
 
     @Test
     public void testUserGetPassword(){
-        User user1 = new User( "Admin", "tim_cook",  "123456");
+        User user1 = new User( "1","Admin", "tim_cook",  "123456");
         assertEquals("Testing User's getAccountPassword", "123456",user1.getPassword());
     }
 
